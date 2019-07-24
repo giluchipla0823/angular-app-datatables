ead0e11f74f626ce98b2a32d89a6fe8f9a05c351
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2019-07-24 08:12:52 +0200

    Avances en funcionalidad de datatables

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -162,6 +162,6 @@ export class BooksComponent implements AfterViewInit, OnInit {
   }
 
   accept(){
-    console.log('Aceptando...');
+    console.log('Aceptando...', this.modalRef.content.data);
   }
 }
